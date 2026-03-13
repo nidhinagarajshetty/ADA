8ee88654ac61b8e24e40d88bbb7e088db89832c3
Nidhi Shetty <nidhishetty035@gmail.com>
2026-03-13 12:10:48 +0530

    Add files via upload

---
+++ lab2.py
@@ -0,0 +1,36 @@
+def topological_sort(n, edges):
+    adj = [[0]*n for _ in range(n)]
+    indegree = [0]*n
+
+    for u, v in edges:
+        adj[u][v] = 1
+        indegree[v] += 1
+
+    order = []
+
+    for _ in range(n):
+        for i in range(n):
+            if indegree[i] == 0:
+                order.append(i)
+                indegree[i] = -1
+                for j in range(n):
+                    if adj[i][j] == 1:
+                        indegree[j] -= 1
+                break
+        else:
+            print("Cycle detected")
+            return
+
+    print("Topological Order:", order)
+
+
+# Main
+n = int(input("Enter number of vertices: "))
+e = int(input("Enter number of edges: "))
+
+edges = []
+for _ in range(e):
+    u, v = map(int, input().split())
+    edges.append((u, v))
+
+topological_sort(n, edges)
